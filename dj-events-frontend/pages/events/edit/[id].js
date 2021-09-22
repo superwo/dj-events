0ec055412a2109fe11be8a088421bb37efa3f5e4
Radu <radu.avadanii@bludelego.it>
2021-09-22 10:21:16 +0300

    changes

--- dj-events-frontend/pages/events/edit/[id].js
+++ dj-events-frontend/pages/events/edit/[id].js
@@ -10,6 +10,7 @@ import { API_URL } from "@/config/index";
 import styles from "@/styles/Form.module.css";
 import moment from "moment";
 import { FaImage } from "react-icons/fa";
+import ImageUpload from "@/components/ImageUpload";
 
 export default function EditEventPage({ evt }) {
   const [values, setValues] = useState({
@@ -59,6 +60,13 @@ export default function EditEventPage({ evt }) {
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setValues({ ...values, [name]: value });
+  };
+
+  const imageUploaded = async (e) => {
+    const res = await fetch(`${API_URL}/events/${evt.id}`);
+    const data = await res.json();
+    setImagePreview(data.image.formats.thumbnail.url);
+    setShowModal(false);
   };
 
   return (
@@ -162,7 +170,7 @@ export default function EditEventPage({ evt }) {
       </div>
 
       <Modal show={showModal} onClose={() => setShowModal(false)}>
-        IMAGE UPLOAD
+        <ImageUpload evtId={evt.id} imageUploaded={imageUploaded} />
       </Modal>
     </Layout>
   );
